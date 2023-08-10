dda31b92c48aefa528cc1b77b6b6eac9772faa13
Rhabad <nicolaspizarro.450@gmail.com>
2023-08-10 19:06:32 -0400

    tiktaktoe

---
+++ src/Librerias/TictactoeAPP.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Librerias;
+
+import Vistas.TicTacToe;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class TictactoeAPP {
+    public static void main(String[] args) {
+        TicTacToe tic = new TicTacToe();
+        
+        tic.setVisible(true);
+        tic.setResizable(false);
+        tic.setLocationRelativeTo(null);
+    }
+}
